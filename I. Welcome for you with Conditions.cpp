2e4dd30499a407b1aa084c2b85771532b7704fba
Kamrul Hasan <hasankamrulhp@gmail.com>
2023-04-27 06:56:41 +0600

    Codeforces

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a, b;
+    cin>> a>> b;
+    if(a>=b) cout<<"Yes"<<endl;
+    else cout << "No" << endl;
+    return 0;
+}
---
+++ L. The Brothers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   string name1, name2, name3, name4;
+    cin>> name1 >> name2;
+    cin>> name3 >> name4;
+
+    if(name2 == name4){
+        cout << "ARE Brothers"<< endl;
+    }
+    else cout << "NOT"<< endl;
+
+    
+    return 0;
+}
