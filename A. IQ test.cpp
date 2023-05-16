81966ee1f7884d3cf130974f21b6309c4e674f9b
Kamrul Hasan <hasankamrulhp@gmail.com>
2023-05-16 23:46:30 +0600

    codeforce

---
+++ A. IQ test.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v;
+    int a, b, even = 0, odd = 0, res = 0, res1;
+    cin>> a;
+    // for(int i = 0 ; i<a; i++){
+    //     cin >> v[i]; 
+    // }
+
+    // for(int i=0; i<v.size(); i++){
+    //     if(v[i] % 2==0){
+    //         even++;
+    //         sort(v.begin(),  v.end());
+    //         reverse(v.begin(), v.end());
+    //         if(odd < even){
+    //         res1 = odd - v[0];
+    //         cout << res1 << endl;
+    //         }
+    //         else{
+    //         cout << even << endl;
+    //     }
+    // }
+    //     else if(v[i] % 2 != 0){
+    //         odd++;
+    //     }
+        
+
+    // }
+}
+
+    
